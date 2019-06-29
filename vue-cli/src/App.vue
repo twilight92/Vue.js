1eccf3d60499580a6f2aa3fa25ae26c3757e0bed
tnwjd2050 <tnwjd2050@naver.com>
2019-06-29 19:59:20 +0900

    싱글 파일 컴포넌트에 배운 내용 적용하여 새발 시작

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,34 +1,29 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <!-- 컴포넌트 명명법 종류(네이밍 컨벤션) : 케밥기법/파스칼케이스/파스칼케이스 진보 -->
-    <!-- vsCode의 파일 바로가기 기능 사용하려면 케밥기법 사용 -->
-    <hello-world></hello-world>
-    <HelloWorld></HelloWorld>
-    <HelloWorld/>
+  <!-- 템플릿 안에는 html엘리먼트가 최상위에 하나만 있어야한다. : 템플릿 루트는 하나여야 한다. -->
+  <div>
+    {{ str }}
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  /* 
+  new Vue({
+    data: {
+      str: 'hi'
+    }
+  }) 
+  */
 
-export default {
-  // 인스턴스 옵션 속성 or 컴포넌트 옵션 속성
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    data : function(){
+      return {
+        str : 'hi'
+      }
+      //function 내부에 return으로 새 객체 반환해야함
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
