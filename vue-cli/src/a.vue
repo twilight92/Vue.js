19d2b4b300653d78f0f3ce2ec9cbb0d6827d5dc6
tnwjd2050 <tnwjd2050@naver.com>
2019-06-29 19:45:00 +0900

    폴더 구조 변경

---
+++ vue-cli/src/a.vue
@@ -0,0 +1,15 @@
+//scaffold
+<template>
+    <!-- HTML -->
+    <div>header</div>
+</template>
+
+<script>
+export default {
+    //Javascript - 인스턴스 옵션
+}
+</script>
+
+<style>
+    /* CSS */
+</style>
